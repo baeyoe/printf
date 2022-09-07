af349a99b16410ce58864428a2fb7d42ce09d5a2
baeyoe <baeyoe@yahoo.com>
2022-09-06 23:25:05 -0700

    another fix

--- get_precision.c
+++ get_precision.c
@@ -12,12 +12,10 @@ int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
 	int precision = -1;
-	
+
 	if (format[curr_i] != '.')
 		return (precision);
-	
 	precision = 0;
-	
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -34,8 +32,6 @@ int get_precision(const char *format, int *i, va_list list)
 		else
 			break;
 	}
-	
 	*i = curr_i - 1;
-	
 	return (precision);
 }
